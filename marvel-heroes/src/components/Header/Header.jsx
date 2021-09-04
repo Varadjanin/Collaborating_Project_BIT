0d82c405698f30482fa77e74fb8ed190d50b55b3
Varadjaninn <nvaradjanin@gmail.com>
2021-09-04 12:13:48 +0200

    adding dark header

--- marvel-heroes/src/components/Header/Header.jsx
+++ marvel-heroes/src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Navbar, Container } from "react-bootstrap";
 
 export class Header extends React.Component {
     render() {
         return(
-            <Container >
-                Hero Team
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
             </Container>
+        </Navbar>
         )
     }
 }
